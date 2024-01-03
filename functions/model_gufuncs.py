9fe943625eb8291a72a4eeffa8e28cc81ca87aab
biopherret <webster.sarah.a@gmail.com>
2024-01-03 15:13:34 -0800

    un-collapsed the constants for readability

--- functions/model_gufuncs.py
+++ functions/model_gufuncs.py
@@ -6,15 +6,8 @@ import numpy as np
 from numba import guvectorize
 import numba
 
-@guvectorize(['float32[:], float32[:], int32[:], float64[:], float64[:]'], '(n), (m), (l), (k)->(k)', target='cuda') #exclude the last input object of the function in the signature otherwise will error
-def get_L_final_vec(parr4, cont_floats, cont_ints, zero_vec, L_vec):
-
-    Nb = cont_ints[0]
-    n_max = cont_ints[1]
-    num_steps = cont_ints[2]
-    N_nucl = cont_ints[3]
-    h = cont_floats[0]
-    Ti = cont_floats[1]
+@guvectorize(['float32[:], float32, float32, int32, int32, int32, int32, float64[:], float64[:]'], '(n), (), (), (), (), (), (), (k)->(k)', target='cuda') #the last argument is the output and therefore cannot be included in the input signature
+def get_L_final_vec(parr4, h, Ti, Nb, n_max, num_steps, N_nucl, zero_vec, L_vec):
 
     found_L_vec = mdf.L(L_vec, zero_vec, parr4, N_nucl, Nb, n_max, num_steps, h, Ti)
     for n in range(n_max +2):
