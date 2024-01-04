1a159c0f2ca8fcc11802d967d981c329dfc8eff1
biopherret <webster.sarah.a@gmail.com>
2024-01-03 16:55:29 -0800

    clean up

--- functions/model_gufuncs.py
+++ functions/model_gufuncs.py
@@ -7,8 +7,8 @@ from numba import guvectorize
 import numba
 
 @guvectorize(['float32[:], float32, float32, int32, int32, int32, int32, float64[:], float64[:]'], '(n), (), (), (), (), (), (), (k)->(k)', target='cuda') #the last argument is the output and therefore cannot be included in the input signature
-def get_L_final_vec(parr4, h, Ti, Nb, n_max, num_steps, N_nucl, zero_vec, L_vec):
+def get_L_final_vec(parr4, h, Ti, Nb, n_max, num_steps, N_nucl, dummy_vec, L_vec):
 
-    found_L_vec = mdf.L(L_vec, zero_vec, parr4, N_nucl, Nb, n_max, num_steps, h, Ti)
+    found_L_vec = mdf.L(L_vec, dummy_vec, parr4, N_nucl, Nb, n_max, num_steps, h, Ti)
     for n in range(n_max +2):
         L_vec[n] = found_L_vec[n]
