04e1fda9be50598ac863edad60f7f61988dd89eb
biopherret <webster.sarah.a@gmail.com>
2024-02-21 14:08:36 -0800

    troubleshootnig results benig slightly different

--- functions/model_gufuncs.py
+++ functions/model_gufuncs.py
@@ -14,14 +14,15 @@ def _get_L_mat(parr, N_nucl, Nb, n_max, num_steps, h, L0_mat, L_mat):
             L_mat[l,n] = L0_mat[l,n]
 
     for t_step in range(1,num_steps):
+        current_L = L_mat[0]
         for n in range(n_max + 2):
-            pkp = mdf.part_kp(L_mat[0], n, Nb, n_max)
-            pkp0 = mdf.part_kp0(L_mat[0], n, N_nucl, Nb, n_max)
-            pkd = mdf.part_kd(L_mat[0], n, N_nucl, Nb, n_max)
-            pkb = mdf.part_kbreak(L_mat[0], n, n_max)
+            pkp = mdf.part_kp(current_L, n, Nb, n_max)
+            pkp0 = mdf.part_kp0(current_L, n, N_nucl, Nb, n_max)
+            pkd = mdf.part_kd(current_L, n, N_nucl, Nb, n_max)
+            pkb = mdf.part_kbreak(current_L, n, n_max)
             L_mat[1,n] = parr[0] * pkp + parr[1] * pkp0 + parr[2] * pkd + parr[3] * pkb
         for n in range(n_max + 2):
-            L_mat[0,n] = mdf.vec_add(L_mat[0], mdf.scal_x_vec(h, L_mat[1]))[n]
+            L_mat[0,n] = mdf.vec_add(current_L, mdf.scal_x_vec(h, L_mat[1]))[n]
 
 def get_L_mat(parr, N_nucl, Nb, n_max, num_steps, h, Ti, Si):
     L0_vec = np.zeros((n_max+2), dtype=np.float64)
